075ec43bf3024a710a221868642ad698491e5531
printer83mph <printer.83mph@gmail.com>
2017-02-07 21:22:57 -0500

    Add minor tweaks and refine opening and saving system

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ window.onload = function() {
   var openProject = document.getElementById("openproject");
 
   openProject.onclick = () => {
-    var files = (dialog.showOpenDialog({properties:["openFile"], filters:[
+    var files = dialog.showOpenDialog({properties:["openFile"], filters:[
       {name:"JSON Project", extensions:["json"]}
-    ]}));
+    ]});
     if(files != undefined) {
       projectLocation = files[0];
       fs.readFile(files[0], (err, data) => {
@@ -23,12 +23,53 @@ window.onload = function() {
       });
     }
   }
+
+	var saveProject = document.getElementById("saveproject");
+
+	saveProject.onclick = () => {
+		if(projectLocation == "") {
+			saveAs();
+		} else {
+			fs.writeFile(projectLocation, JSON.stringify(project));
+		}
+	}
+
+	var saveProjectAs = document.getElementById("saveprojectas");
+
+	saveProjectAs.onclick = saveAs;
+
+  var newProject = document.getElementById("newproject");
+
+  newProject.onclick = () => {
+    project = {"name":"Untitled", "sources":{}, "notecards":{}};
+    document.getElementById("currentproject").innerHTML = project.name;
+  }
+
+  var editName = document.getElementById("editname");
+
+  editName.onclick = () => {
+    project.name = document.getElementById("newname").value;
+    document.getElementById("currentproject").innerHTML = project.name;
+  }
 }
 
 function randomId() {
   var str = "";
-  for(var i = 0; i < 3; i++) {
-    str += Math.floor(Math.random() * 10);
-  }
+	do {
+		for(var i = 0; i < 3; i++) {
+			str += Math.floor(Math.random() * 10);
+		}
+	}
+	while((str in project.sources) || (str in project.notecards))
   return str;
+}
+
+function saveAs() {
+	var filePath = dialog.showSaveDialog({filters:[
+		{name:"JSON Project", extensions:["json"]}
+	]});
+	if(filePath != undefined) {
+		fs.writeFile(filePath, JSON.stringify(project));
+		projectLocation = filePath;
+	}
 }
